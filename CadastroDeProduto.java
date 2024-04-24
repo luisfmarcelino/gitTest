b1f3033acae8ffc760ee007946930d839e3840d5
luisfmarcelino <luismarcelino.shift@gmail.com>
2024-04-24 08:59:45 -0300

    fix: fixed the sout text

--- CadastroDeProduto.java
+++ CadastroDeProduto.java
@@ -5,7 +5,7 @@ public class CadastroDeProduto {
         System.out.println("B");
 
         for(int i=0 ; i<10 ; i++) {
-            System.out.println(i);
+            System.out.println("The number is: "+ i);
         }
     }
 }
