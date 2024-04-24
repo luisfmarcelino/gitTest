a485721201891cca7f708683eaf9beee922b9c0a
luisfmarcelino <luismarcelino.shift@gmail.com>
2024-04-24 08:58:24 -0300

    feat: implemented a sout loop

--- CadastroDeProduto.java
+++ CadastroDeProduto.java
@@ -3,5 +3,9 @@ public class CadastroDeProduto {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("B");
+
+        for(int i=0 ; i<10 ; i++) {
+            System.out.println(i);
+        }
     }
 }
